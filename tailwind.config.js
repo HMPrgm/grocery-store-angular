770448683de1755b3d428a544b98d628adc1ede9
MSI Laptop <henrymunprof@gmail.com>
2024-06-20 18:33:01 -0400

    Edited colors and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        Cwhite: '#E0E1DD',
+        Cblack: '#0D1B2A',
+        Clightblue: '#778DA9',
+        Cblue: '#415A77',
+        Cdarkblue: '#1B263B',
+      }
+    },
   },
   plugins: [],
 }
