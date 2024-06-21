23ba64accbb0afbdbb8dc0e3ef43e6f5fd299fb4
MSI Laptop <henrymunprof@gmail.com>
2024-06-21 14:51:15 -0400

    navbar hamburger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
 import { StorebodyComponent } from './store/storebody/storebody.component';
 import { ProductgroupComponent } from "./store/productgroup/productgroup.component";
 import { FooterComponent } from "./footer/footer.component";
